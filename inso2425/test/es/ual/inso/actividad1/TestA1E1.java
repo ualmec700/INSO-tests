242ed7fcf9a24800f077056c55a04d118d1c2deb
Javier Criado <javi.criado@ual.es>
2024-10-07 11:16:29 +0200

    Tests for A1E1 modified

--- inso2425/test/es/ual/inso/actividad1/TestA1E1.java
+++ inso2425/test/es/ual/inso/actividad1/TestA1E1.java
@@ -39,8 +39,8 @@ public class TestA1E1 {
   private static List<Extend> extendList;
   private static List<Association> associationList;
 
-  private static int nactor = 4;
-  private static int nusecase = 5;
+  private static int nactor = 3;
+  private static int nusecase = 6;
   private static int ngenusecase = 1;
   private static int ngenactor = 1;
   private static int ninclude = 1;
